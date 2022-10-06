499b7301cd0df0c59cbde829956271a234a48083
Saul Julio Teran <sSj>
2022-10-06 09:55:28 -0500

    Continue

--- back/app.js
+++ back/app.js
@@ -1,4 +1,10 @@
-const express=require("express");
-const app = express();
+const express=require("express")
+const app = express()
+app.use(express.json());
+
+//Importar rutas
+const productos=require("./routes/product")
+
+app.use('/api',productos) //Sujeto a decision (ruta del navegador)
 
 module.exports=app
---
+++ back/routes/product.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+
+const {getProducts} = require("../controllers/productsController")
+router.route('/producto').get(getProducts)
+
+module.exports=router
