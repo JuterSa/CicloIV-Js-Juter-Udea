5937faa8e6d88f0c90eb172a970d3897cd9ae111
Saul Julio Teran <sSj>
2022-10-13 08:44:46 -0500

    Modificaciones react

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 
@@ -7,18 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Tienda de mascotas Juter Shop</h1>
+      <img src="./images/buldog.png" className="App-logo" alt="logo" />
+    
       </header>
     </div>
   );
