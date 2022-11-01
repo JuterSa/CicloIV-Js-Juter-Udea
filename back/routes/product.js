a063b2feb56ed343b5fe7da78dd4cfcf31382485
Saul Julio Teran <sSj>
2022-11-01 08:37:30 -0500

    Autenticacion clase 1 de noviembre

--- back/routes/product.js
+++ back/routes/product.js
@@ -1,12 +1,15 @@
-const express = require("express")
-const router = express.Router()
+const express=require("express")
+const router=express.Router();
 
-const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController")
-router.route('/productos').get(getProducts) //lista
-router.route('/producto/nuevo').post(newProduct) //Nuevo
-router.route('/producto/:id').get(getProductById) //Consulta por id
-router.route('/producto/:id').put(updateProduct) //Creacion de la ruta de actualizacion
-router.route('/producto/:id').delete(deleteProduct) //Eliminar
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
+const { isAuthenticatedUser , authorizeRoles} = require("../middleware/auth");
 
+//Probemos autenticación
+router.route('/productos').get(isAuthenticatedUser, authorizeRoles("admin"), getProducts)  //Establecemos desde que ruta queremos ver el getProducts
+router.route('/producto/nuevo').post(newProduct); //establecemos la ruta
+router.route('/producto/:id').get(getProductById); //Ruta para consultar por id
+router.route('/producto/:id').put(updateProduct);//Creacion de la ruta de actualizacion
+router.route('/producto/:id').delete(deleteProduct); //Creacion de la ruta de eliminacion por id
+ 
 
-module.exports=router
+module.exports=router;
